3cd9eb2fa28631d43b86a1c635e02ae1288c7c25
Alex Yakimenko <alexyakimenkojr@gmail.com>
2024-02-10 17:38:12 +0200

    implement wordFrequency function without return value yet

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ function isPangram(sentence) {
   const charSet = new Set(letters);
   return charSet.size === 26;
 }
+
+function wordFrequency(text) {
+  const words = text.split(" ");
+  const map = new Map();
+
+  for (let word of words) {
+    if (map.has(word)) {
+      let count = map.get(word);
+      map.set(word, count + 1);
+      continue;
+    }
+
+    map.set(word, 1);
+  }
+
+  const sorted = Array.from(map).sort((a, b) => b[1] - a[1]);
+
+  console.log(sorted.slice(0, 3));
+}
+
+wordFrequency(
+  "This is a sample text. It is just a sample to demonstrate word frequency analysis."
+);
