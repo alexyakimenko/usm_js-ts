cd4838838bf22a5b266c0b8a53a0edb3684b14ad
Alex Yakimenko <alexyakimenkojr@gmail.com>
2024-03-01 19:41:04 +0200

    make rest tasks except one

--- individual_work/main.js
+++ individual_work/main.js
@@ -1,14 +1,24 @@
 const transactions = require("./transaction.json");
 
+const Stringify = {
+  string: function () {
+    return JSON.stringify(this);
+  },
+};
+
 class TransactionAnalyzer {
   #transactions = [];
 
   constructor(transactions) {
     this.#transactions = transactions;
+
+    for (let transaction of this.#transactions) {
+      Object.setPrototypeOf(transaction, Stringify);
+    }
   }
 
-  addTransaction() {
-    transactions.push({}); // todo: add transaction fields
+  addTransaction(transaction) {
+    transactions.push(transaction);
   }
 
   getAllTransactions() {
@@ -30,7 +40,7 @@ class TransactionAnalyzer {
     );
   }
 
-  calculateTotalAmountByDate() {} // to be clarified!
+  calculateTotalAmountByDate(year, month, day) {} // to be clarified!
 
   getTransactionsByType(type) {
     return transactions.filter(
@@ -38,7 +48,15 @@ class TransactionAnalyzer {
     );
   }
 
-  getTransactionsInDateRange(startDate, endDate) {} // todo:
+  getTransactionsInDateRange(startDate, endDate) {
+    return transactions.filter((transaction) => {
+      const transactionDate = new Date(transaction.transaction_date);
+      return (
+        transactionDate.getTime() > startDate.getTime() &&
+        transactionDate.getTime() < endDate.getTime()
+      );
+    });
+  }
 
   getTransactionsByMerchant(merchantName) {
     return transactions.filter(
@@ -68,13 +86,102 @@ class TransactionAnalyzer {
     );
   }
 
-  findMostTransactionsMonth() {} // to be clarified!
+  findMostTransactionsMonth() {
+    const month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
-  findMostDebitTransactionsMonth() {} // to be clarified!
+    const map = new Map();
 
-  mostTransactionTypes() {} // to be clarified!
+    for (let transaction of transactions) {
+      const transactionMonth =
+        month[new Date(transaction.transaction_date).getMonth()];
 
-  getTransactionsBeforeDate(date) {} // todo:
+      if (map.has(transactionMonth)) {
+        let count = map.get(transactionMonth);
+        map.set(transactionMonth, count + 1);
+        continue;
+      }
+      map.set(transactionMonth, 1);
+    }
+
+    const sorted = Array.from(map).sort((a, b) => b[1] - a[1]);
+
+    return sorted[0][0];
+  }
+
+  // todo: reduce boilerplate
+  findMostDebitTransactionsMonth() {
+    const month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const map = new Map();
+
+    for (let transaction of transactions) {
+      if (transaction.transaction_type !== "debit") continue;
+      const transactionMonth =
+        month[new Date(transaction.transaction_date).getMonth()];
+
+      if (map.has(transactionMonth)) {
+        let count = map.get(transactionMonth);
+        map.set(transactionMonth, count + 1);
+        continue;
+      }
+      map.set(transactionMonth, 1);
+    }
+
+    const sorted = Array.from(map).sort((a, b) => b[1] - a[1]);
+
+    return sorted[0][0];
+  }
+
+  mostTransactionTypes() {
+    const map = new Map();
+
+    for (let transaction of transactions) {
+      const type = transaction.transaction_type;
+      if (map.has(type)) {
+        let count = map.get(type);
+        map.set(type, count + 1);
+        continue;
+      }
+      map.set(type, 1);
+    }
+
+    const sorted = Array.from(map).sort((a, b) => b[1] - a[1]);
+
+    return sorted[0][0];
+  }
+
+  getTransactionsBeforeDate(date) {
+    return transactions.filter((transaction) => {
+      const transactionDate = new Date(transaction.transaction_date);
+      return transactionDate.getTime() < date.getTime();
+    });
+  }
 
   findTransactionById(id) {
     return transactions.find(
@@ -87,7 +194,11 @@ class TransactionAnalyzer {
       (transaction) => transaction.transaction_description
     );
   }
+
+  test() {
+    console.log(transactions[0].string());
+  }
 }
 
 const analyzer = new TransactionAnalyzer(transactions);
-console.log(analyzer.mapTransactionDescriptions());
+console.log(analyzer.test());
